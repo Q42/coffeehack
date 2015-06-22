fbafc517042add0f1984a3558ff92b2b06e043f0
Taco Ekkel <taco@Tacos-MacBook-Pro.local>
2015-06-22 10:13:08 +0200

    first test version

--- jura.py
+++ jura.py
@@ -1,18 +1,43 @@
+# protocol source: http://protocol-jura.do.am/index/protocol_to_coffeemaker/0-7
+
 import sys
 import serial
 import time
+import struct
 
-def encode(str):
-    # Information only in bit 2 and 5, all other bits allways 1!
-    # 4 UART bytes are one real byte:
-    # MSB.....LSB
-    # UART byte 33221100
-    # bit 52525252
-    return str
+# encode a byte to jura-encoding
+# Information only in bit 2 and 5, all other bits are 1
+# 4 UART bytes are one real byte:
+# MSB.....LSB
+# UART byte 33221100
+# bit 52525252
+def encodebyte(bin):
+    bs = 0xDADADADA # template
+
+    b = struct.unpack("B", bin)[0]
+
+    bs |= ((b & 0x80) >> 2) << 24
+    bs |= ((b & 0x40) >> 4) << 24
+    bs |= ((b & 0x20)) << 16
+    bs |= ((b & 0x10) >> 2) << 16
+    bs |= ((b & 0x08) << 2) << 8
+    bs |= ((b & 0x04)) << 8
+    bs |= ((b & 0x02) << 4)
+    bs |= ((b & 0x01) << 2)
+
+    return chr(bs & 0xFF000000 >> 24) + chr(bs & 0x00FF0000 >> 16) + chr(bs & 0x0000FF00 >> 8) + chr(bs & 0x000000FF)
+
+cmdstr = "RE:31\r\n"
 
 serialport = serial.Serial('/dev/ttyAMA0', 9600)
 serialport.open()
 
+# write machine code
+for c in cmdstr:
+    serialport.write(encodebyte(c))
+    time.sleep(8) # 8ms break between bytegroups
+
+# show results
 try:
     while true:
         response = serialport.readline()
