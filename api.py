19cb17599f8ae64c48383b928dd2941338944e4b
Guido Bouman <guido@q42.nl>
2015-07-01 17:56:47 +0200

    Change to dynamic payload.

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
-import requests, json
+import requests, json, time
 
 url = 'http://iot-api.scalingo.io/api/coffeecups'
 data = json.dumps({
-    'type': 'latte',
-    'timestamp': 1435762371
+    'type': 'coffee',
+    'timestamp': int(time.time())
 })
 response = requests.post(url, data)
 print response.json()
