a9aea21f69f6bf758cf02fb850a47e60cff72e12
Taco Ekkel <taco@q42.nl>
2015-09-24 13:46:25 +0200

    cleanup + final photon code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # coffeehack
+
+`coffeehack.ino` is the arduino/squirrel-code that runs the local interface to the machine's maintenance port. Run it on a Spark Photon. `webhook-coffee.json` is what you'll need to `particle webhook create` in order for the Photon's event trigger to cause a web requests to the API.
+
+`test-app` shows how to get data out of the API (over DDP)
+
+`reverse-engineering` has info on the machine, the v? files are EEPROM dumps.
+
+(`jura.py` is an earlier attempt in python on a raspberry Pi.)
--- coffeehack.ino
+++ coffeehack.ino
@@ -1,6 +1,12 @@
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
+// SoftwareSerial mySerial(10, 11); // RX TX
 
-SoftwareSerial mySerial(10, 11); // RX TX
+#ifndef bitRead
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+#define bitSet(value, bit) ((value) |= (1UL << (bit)))
+#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
+#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
+#endif
 
 byte z0, z1, z2, z3;
 byte x0, x1, x2, x3, x4;
@@ -8,12 +14,17 @@ byte d0, d1, d2, d3;
 byte intra = 1, inter = 7;
 String hexval;
 
+// TODO: melkkoffie, twee koffie, twee espressi
+// TODO: make this an array and iterate all functions over it
+int t_koffie, t_ristretto, t_cappuccino, t_espresso, t_latte_macchiato, t_macchiato;
+int o_koffie, o_ristretto, o_cappuccino, o_espresso, o_latte_macchiato, o_macchiato;
+
 void setup() {
   Serial.begin(9600);
-  mySerial.begin(9600);
+  Serial1.begin(9600);
+  Serial.println("Starting Q42 coffeehacker");
 }
 
-// read a Jura counter from EEPROM, given a memory offset
 int getCounter(int offset)
 {
   if(offset > 0x800) return -1;
@@ -29,21 +40,19 @@ int getCounter(int offset)
   toCoffeemaker(hexval[1]); delay(inter);
   toCoffeemaker(hexval[2]); delay(inter);
   toCoffeemaker(0x0D); delay(inter);
-  toCoffeemaker(0x0A); delay(100); //  allow for processing time to prevent
-                                   // garbage from still sitting on the serial buffer
+  toCoffeemaker(0x0A); delay(100);
 
   String r = "";
 
-  while(mySerial.available()) {
-    delay (intra); d0 = mySerial.read();
-    delay (intra); d1 = mySerial.read();
-    delay (intra); d2 = mySerial.read();
-    delay (intra); d3 = mySerial.read();
+  while(Serial1.available()) {
+    delay (intra); d0 = Serial1.read();
+    delay (intra); d1 = Serial1.read();
+    delay (intra); d2 = Serial1.read();
+    delay (intra); d3 = Serial1.read();
     delay (inter);
     r += char(fromCoffeemaker(d0,d1,d2,d3));
   }
 
-  // r should be in the form: 're:XXXX\r\n', 9 bytes
   if (r.length() == 9) {
     String hex = r.substring(3,7);
     int number = (int)strtol(hex.c_str(), NULL, 16);
@@ -54,22 +63,42 @@ int getCounter(int offset)
 }
 
 void loop() {
+  o_koffie = t_koffie;
+  o_ristretto = t_ristretto;
+  o_cappuccino = t_cappuccino;
+  o_espresso = t_espresso;
+  o_latte_macchiato = t_latte_macchiato;
+  o_macchiato = t_macchiato;
+
   Serial.println("------ reading values");
-  int t_koffie = getCounter(0x282);
-  int t_ristretto = getCounter(0x281);
-  int t_cappuccino = getCounter(0x284);
-  int t_espresso = getCounter(0x280);
-  int t_latte_macchiato = getCounter(0x285);
-  int t_macchiato = getCounter(0x286);
+  t_koffie = getCounter(0x282);
+  t_ristretto = getCounter(0x281);
+  t_cappuccino = getCounter(0x284);
+  t_espresso = getCounter(0x280);
+  t_latte_macchiato = getCounter(0x285);
+  t_macchiato = getCounter(0x286);
+
   Serial.print("koffie:.........."); Serial.println(t_koffie);
   Serial.print("cappuccino:......"); Serial.println(t_cappuccino);
   Serial.print("espresso:........"); Serial.println(t_espresso);
   Serial.print("ristretto:......."); Serial.println(t_ristretto);
   Serial.print("latte macchiato:."); Serial.println(t_latte_macchiato);
   Serial.print("macchiato:......."); Serial.println(t_macchiato);
-  // TODO: melkkoffie, twee koffie, twee espressi
+
+  if(t_koffie == o_koffie + 1) trigger("koffie");
+  if(t_ristretto == o_ristretto + 1) trigger("ristretto");
+  if(t_cappuccino == o_cappuccino + 1) trigger("cappuccino");
+  if(t_espresso == o_espresso + 1) trigger("espresso");
+  if(t_latte_macchiato == o_latte_macchiato + 1) trigger("latte_macchiato");
+  if(t_macchiato == o_macchiato + 1) trigger("macchiato");
+
   delay(5 * 1000);
-  // TODO: detect differences, cloud API call
+}
+
+void trigger(String ctype)
+{
+  Serial.println(" -- triggering webhook for a " + ctype);
+  Spark.publish("coffeecups", ctype, 60, PRIVATE);
 }
 
 byte fromCoffeemaker(byte x0, byte x1, byte x2, byte x3) {
@@ -99,9 +128,9 @@ byte toCoffeemaker(byte z) {
   bitWrite(z3, 2, bitRead(z,6));
   bitWrite(z3, 5, bitRead(z,7));
 
-  delay(intra); mySerial.write(z0);
-  delay(intra); mySerial.write(z1);
-  delay(intra); mySerial.write(z2);
-  delay(intra); mySerial.write(z3);
+  delay(intra); Serial1.write(z0);
+  delay(intra); Serial1.write(z1);
+  delay(intra); Serial1.write(z2);
+  delay(intra); Serial1.write(z3);
   delay(inter);
 }
