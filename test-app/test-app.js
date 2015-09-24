b6fe1d3c04b71960985dc9831f0dbfd23f56bf56
Taco Ekkel <taco@q42.nl>
2015-09-24 14:17:12 +0200

    updated test app for cases to use on Q site

--- test-app/test-app.js
+++ test-app/test-app.js
@@ -7,13 +7,32 @@ if (Meteor.isClient) {
       return coffeecups.find().count();
     },
     counter_020: function () {
-      return coffeecups.find({"location": "Q020"}).count();
+      return coffeecups.find({"location": "q020"}).count();
     },
     counter_070: function () {
-      return coffeecups.find({"location": "Q070"}).count();
+      return coffeecups.find({"location": "q070"}).count();
+    },
+    daycounter_020: function() {
+      d = new Date();
+      d.setUTCHours(0,0,0,0);
+      return coffeecups.find(
+        {
+          published_at: { $gte: d.toISOString() },
+          location: "q020"
+        }
+      ).count();
+    },
+    first_020: function() {
+      return coffeecups.findOne({
+                published_at: { $gte: d.toISOString() },
+                location: "q020"
+              });
+    },
+    latest_020: function() {
+      return coffeecups.findOne({ location: "q020" }, { sort: { published_at: -1 }});
     },
     latest: function () {
-      return coffeecups.findOne({}, { sort: { timestamp: -1 }});
+      return coffeecups.findOne({}, { sort: { published_at: -1 }});
     }
   });
 }
